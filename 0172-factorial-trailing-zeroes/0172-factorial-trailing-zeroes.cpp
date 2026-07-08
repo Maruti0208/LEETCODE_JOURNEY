6a5355633c3a36606950e3e6b61587a360491585
Maruti_0208 <marutigaikwad2408@gmail.com>
2026-07-08 23:45:59 +0530

    Time: 0 ms (100%), Space: 8 MB (86.84%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int a=0,i=0;
+        while(n>0){
+            a+=n/5;
+            n/=5;
+            
+        }
+        return a;
+        
+    }
+};
