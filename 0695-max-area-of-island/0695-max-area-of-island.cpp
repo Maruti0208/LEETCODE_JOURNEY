2b8b93abd7ab4e88e7804b3e2225979549408aa1
Maruti_0208 <marutigaikwad2408@gmail.com>
2026-08-11 19:16:20 +0530

    Time: 0 ms (100%), Space: 27.4 MB (79.06%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& g, vector<vector<int>>& v, int i, int j) {
+    int dfs(vector<vector<int>>& g, int i, int j) {
         int n = g.size();
         int m = g[0].size();
-         
-        if (i < 0 || i >= n || j < 0 || j >= m || g[i][j] == 0 || v[i][j])
+        if (i >= n || i < 0 || j < 0 || j >= m || g[i][j] == 0)
             return 0;
- v[i][j] = 1;
-      
+        g[i][j] = 0;
 
-         
-
-        return 1 + dfs(g, v, i + 1, j) + dfs(g, v, i - 1, j) +
-               dfs(g, v, i, j + 1) + dfs(g, v, i, j - 1);
+        return 1+dfs(g, i + 1, j) + dfs(g, i, j + 1) +
+               dfs(g, i - 1, j) + dfs(g, i, j - 1);
     }
     int maxAreaOfIsland(vector<vector<int>>& g) {
         int n = g.size();
         int m = g[0].size();
         int ans = 0;
-        vector<vector<int>> v(n, vector<int> (m,0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (g[i][j] == 1 && v[i][j] == 0)
-                    ans = max(dfs(g, v, i, j),ans);
+                if (g[i][j] == 1) {
+                    int a = dfs(g, i, j);
+                    ans = max(ans, a);
+                }
             }
         }
         return ans;
