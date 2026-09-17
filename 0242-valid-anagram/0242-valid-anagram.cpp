bf12d9a0b622f06f9ed52b82ec9c9b6583e430bc
Maruti_0208 <marutigaikwad2408@gmail.com>
2026-09-17 19:57:56 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!=t.size()) return 0;
+        int n=s.size();
+        vector<int> a(26);
+        for(int i=0;i<n;i++){
+            a[s[i]-'a']++;
+            a[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i]!=0) return 0;
+        }
+        return 1;
     }
 };
