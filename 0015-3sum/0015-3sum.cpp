da141da5d9c0de91f3d261ec837bddda43ccd9c0
Maruti_0208 <marutigaikwad2408@gmail.com>
2026-09-19 02:11:07 +0530

    Time: 111 ms (11.2%), Space: 38.2 MB (9.95%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         set<vector<int>> s;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
-            // if(nums[i]==nums[i-1]) continue;
+            if(i>0&& nums[i]==nums[i-1])continue;
             int j=i+1;
             int k=n-1;
             while(j<k){
